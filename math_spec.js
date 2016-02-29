38a3b98bba4a5446dea0c2aff1212a020ec527a0
Victoria Ciavarra <Victoria Ciavarra>
2016-02-29 08:51:41 -0500

    added tests/code for math

--- math_spec.js
+++
@@ -1,9 +0,0 @@
-//tell Jasmine where to find our code 
-var math = require('../math');
-
-describe("A program that does basic arithmetic", function() {
-	it("can add two numbers together", function() {
-		expect(math.add(3,5)).toBe(8);
-	});
-});
-
--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,23 @@
+var math = require('../math');
+
 describe("A program that does basic arithmetic", function() {
+	
 	it("can add two numbers together", function() {
-		expect(add(3,5)).toBe(8);
+		expect(math.add(3,5)).toBe(8);
 	});
+
+it("can subtract two numbers together", function() {
+		expect(math.subtract(3,5)).toBe(-2);
+	});
+
+
+it("can multiply two numbers together", function() {
+		expect(math.multiply(3,5)).toBe(15);
+	});
+
+
+it("can divide two numbers together", function() {
+		expect(math.divide(3,5)).toBe(0.6);
+	});
+
 });
